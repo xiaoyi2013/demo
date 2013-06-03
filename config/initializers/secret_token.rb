385c3680898e92808f2818b5b810d20c3f7bca25
xiaoyi2013 <yiding2020@gmail.com>
2013-06-03 14:00:39 +0800

    ignore doc directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'aef7f78931c0a919ca4bc5f5c800baadb3d218ff10a5836cb1fdfbbff4ac13b7b01aaab64619c373e44ce3a9a832170a62a122218e52ae8ef222ba93b14c3bed'
